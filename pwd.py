5f401ec53b714fe56d75667cd87ea3dbe17557ce
longvd <vuduclong0309@gmail.com>
2025-10-09 20:52:23 +0700

    Rename file to remove invalid character for Windows

---
+++ pwd.py
@@ -0,0 +1,10 @@
+﻿"""Lightweight stub of the Unix-only pwd module for Windows environments."""
+import getpass
+import os
+from collections import namedtuple
+
+_StructPwd = namedtuple("struct_passwd", "pw_name pw_passwd pw_uid pw_gid pw_gecos pw_dir pw_shell")
+
+def getpwuid(uid: int) -> _StructPwd:  # type: ignore[name-defined]
+    username = os.environ.get("USERNAME") or getpass.getuser() or "unknown"
+    return _StructPwd(username, None, uid, 0, "", "", "")
